ffd1fff935607a7cd3f4438fb1b8ee78224b2b45
Ronnie <ronnievelz@gmail.com>
2026-03-23 11:07:24 +0100

    Artist entity created

---
+++ src/main/java/com/my_band_lab/my_band_lab/entity/Artist.java
@@ -0,0 +1,54 @@
+package com.my_band_lab.my_band_lab.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "artists")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true)
+    private User user;
+
+    private String stageName;
+
+    private String biography;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "genre")
+    private MusicGenre genre;
+
+    private boolean verified;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
